69235570f047033e461317828c2065c282fa89f5
minhtran <trcongminh.261002@gmail.com>
2025-03-11 10:17:10 +0700

    add error handling middleware and integrate it into server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,8 @@ import type {NextFunction, Request, Response} from "express";
 import cors from "cors";
 import session from "cookie-session";
 import { config } from "~/configs/app.config";
-import connectDatabase from "./configs/database.config";
+import connectDatabase from "~/configs/database.config";
+import { errorHandler } from "~/middlewares/errorHandler.middleware";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -31,6 +32,8 @@ app.use(cors({
 app.use(`${BASE_PATH}`, (req: Request, res: Response, next: NextFunction) => {
     res.send("Hello World");
 });
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
     console.log(`Server is running on http://localhost:${config.PORT}`);
---
+++ server/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,25 @@
+import { ErrorRequestHandler } from "express";
+import { NextFunction, Request, Response } from "express";
+import { HttpStatus } from "~/constants/httpStatus";
+import { Messages } from "~/constants/message";
+
+export const errorHandler: ErrorRequestHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  console.error(`Error occurred on Path ${req.path} `, error);
+
+  if(error instanceof SyntaxError){
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      message: Messages.INVALID_INPUT,
+      error: error?.message || "Invalid JSON payload",
+    });
+  }
+
+  return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+    message: Messages.SERVER_ERROR,
+    error: error?.message || "Unknown error occurred",
+  });
+}
