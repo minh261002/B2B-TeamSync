9749cd003d4fa627ade0b40a8468bf64512b9f2a
minhtran <trcongminh.261002@gmail.com>
2025-03-11 09:25:16 +0700

    add user status, role constants and HTTP status codes; include message constants for user feedback

---
+++ server/src/constants/enum.ts
@@ -0,0 +1,17 @@
+const UserStatus = {
+  ACTIVE: 'active',
+  INACTIVE: 'inactive',
+  BLOCK: 'block',
+};
+
+const UserRole = {
+  AGENT: 'agent',
+  USER: 'user',
+}
+
+const ActiveStatus = {
+  ACTIVE: 'active',
+  INACTIVE: 'inactive',
+}
+
+export { UserStatus, UserRole,  ActiveStatus };
